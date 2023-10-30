63e6e641ffc97b6c0fbb8fac858725de85e9495c
bdossantos <brad.dsp@gmail.com>
2023-10-30 17:49:39 +0100

    🧙‍♂️ add possibility sending email from contact 🧙‍♂️

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'contact.index',methods: ['GET','POST'])]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager,MailerInterface $mailer): Response
     {
         $contact=new Contact();
         if ($this->getUser()) {
@@ -26,6 +29,23 @@ class ContactController extends AbstractController
             $contact=$form->getData();
             $manager->persist($contact);
             $manager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('admin@recipe.com')
+                ->subject($contact->getSubject())
+
+                // path of the Twig template to render
+                ->htmlTemplate('emails//contact.html.twig')
+
+                // pass variables (name => value) to the template
+                ->context([
+                    'contact'=>$contact
+                ]);
+//                ->html($contact->getMessage());
+
+            $mailer->send($email);
+//            dd($mailer);
             $this->addFlash(
                 'success',
                 'Votre demande a été envoyé avec succès !'
