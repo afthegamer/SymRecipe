3d654b352523e745a1631f47b4875501646e6ee3
braddos <brad.dsp@gmail.com>
2023-11-09 01:20:20 +0100

    ✨  created service MailService.php

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,19 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Service\MailService;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'contact.index',methods: ['GET','POST'])]
-    public function index(Request $request, EntityManagerInterface $manager,MailerInterface $mailer): Response
+    public function index(
+        Request $request,
+        EntityManagerInterface $manager,
+        MailService $mailService
+    ): Response
     {
         $contact=new Contact();
         if ($this->getUser()) {
@@ -30,22 +32,15 @@ class ContactController extends AbstractController
             $manager->persist($contact);
             $manager->flush();
 
-            $email = (new TemplatedEmail())
-                ->from($contact->getEmail())
-                ->to('admin@recipe.com')
-                ->subject($contact->getSubject())
+            $mailService->sendEmail(
+                $contact->getEmail(),
+                'admin@recipe.com',
+                $contact->getSubject(),
+                'emails/contact.html.twig',
+                ['contact'=>$contact]
+            );
 
-                // path of the Twig template to render
-                ->htmlTemplate('emails//contact.html.twig')
 
-                // pass variables (name => value) to the template
-                ->context([
-                    'contact'=>$contact
-                ]);
-//                ->html($contact->getMessage());
-
-            $mailer->send($email);
-//            dd($mailer);
             $this->addFlash(
                 'success',
                 'Votre demande a été envoyé avec succès !'
