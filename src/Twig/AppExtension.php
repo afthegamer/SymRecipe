ed3433280eaa6ef5ef78c7c199227b1ad6ec88fc
bdossantos <brad.dsp@gmail.com>
2023-11-13 17:59:08 +0100

    🧙‍♂️ creat twig filter  🧙‍♂️

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('min_to_hour', [$this, 'minutesToHours'])
+        ];
+    }
+
+    public function minutesToHours($value): string
+    {
+
+        if ($value < 60 || !$value) {
+            return $value;
+        }
+
+        $hours = floor($value / 60);
+        $minutes = $value % 60;
+
+        if ($minutes < 10) {
+            $minutes = '0' . $minutes;
+        }
+
+        return sprintf('%sh%s', $hours, $minutes);
+    }
+}
