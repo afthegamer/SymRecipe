b56b1ecc7cde911ff941015e6ab8a2cfc754e386
bdossantos <brad.dsp@gmail.com>
2023-11-13 15:08:10 +0100

    🧙‍♂️ add testunit  🧙‍♂️

--- Makefile
+++ Makefile
@@ -77,3 +77,11 @@ start:
 stop:
 	symfony serve:stop
 	docker compose stop
+.PHONY: tests
+tests:
+	php bin/console d:d:d --force --if-exists --env=test
+	php bin/console d:d:c --env=test
+	php bin/console d:m:m --no-interaction --env=test
+	php bin/console d:f:l --no-interaction --env=test
+	php bin/phpunit --testdox tests/Unit/
+	php bin/phpunit --testdox tests/Functional/
--- tests/Functional/LoginTest.php
+++ tests/Functional/LoginTest.php
@@ -1,57 +1,58 @@
 <?php
-//
-//namespace App\Tests\Functional;
-//
-//use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-//use Symfony\Component\HttpFoundation\Response;
-//
-//class LoginTest extends WebTestCase
-//{
-//    public function testIfLoginIsSuccessful(): void
-//    {
-//        $client = static::createClient();
-//
-//        /** @var UrlGeneratorInterface $urlGenerator */
-//        $urlGenerator = $client->getContainer()->get("router");
-//
-//        $crawler = $client->request('GET', $urlGenerator->generate('security.login'));
-//
-//        $form = $crawler->filter("form[name=login]")->form([
-//            "_username" => "admin@symrecipe.fr",
-//            "_password" => "password"
-//        ]);
-//
-//        $client->submit($form);
-//
-//        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
-//
-//        $client->followRedirect();
-//
-//        $this->assertRouteSame('home.index');
-//    }
-//
-//    public function testIfLoginFailedWhenPasswordIsWrong(): void
-//    {
-//        $client = static::createClient();
-//
-//        /** @var UrlGeneratorInterface $urlGenerator */
-//        $urlGenerator = $client->getContainer()->get("router");
-//
-//        $crawler = $client->request('GET', $urlGenerator->generate('security.login'));
-//
-//        $form = $crawler->filter("form[name=login]")->form([
-//            "_username" => "admin@symrecipe.fr",
-//            "_password" => "password_"
-//        ]);
-//
-//        $client->submit($form);
-//
-//        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
-//
-//        $client->followRedirect();
-//
-//        $this->assertRouteSame('security.login');
-//
-//        $this->assertSelectorTextContains("div.alert-danger", "Invalid credentials.");
-//    }
-//}
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class LoginTest extends WebTestCase
+{
+    public function testIfLoginIsSuccessful(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+
+        $crawler = $client->request('GET', $urlGenerator->generate('security.login'));
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "_username" => "admin@symrecipe.fr",
+            "_password" => "pass"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertRouteSame('home.index');
+    }
+
+    public function testIfLoginFailedWhenPasswordIsWrong(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+
+        $crawler = $client->request('GET', $urlGenerator->generate('security.login'));
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "_username" => "admin@symrecipe.fr",
+            "_password" => "pass"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertRouteSame('security.login');
+
+        $this->assertSelectorTextContains("div.alert-danger", "Invalid credentials.");
+    }
+}
